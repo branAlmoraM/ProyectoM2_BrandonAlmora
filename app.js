5b7b095328b8d3fdccb93dd3935dc0a31254e8a3
branAlmoraM <brandonalmora@gmail.com>
2026-05-19 19:40:04 -0600

    Rutas: agrega ruta raíz en la aplicación

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const app = express();
 // Middleware para parsear JSON
 app.use(express.json());
 
+// Ruta raíz
+// Ruta raíz
+app.get("/", (req, res) => {
+  res.json({
+    message: "Blog API",
+    endpoints: {
+      authors: "/blog/authors",
+      posts: "/blog/posts",
+    },
+  });
+});
+
 app.use("/blog/authors", authorRoutes);
 app.use("/blog/posts", postsRoutes);
 
